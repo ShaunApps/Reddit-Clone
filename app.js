ae80693633fc17fa7fbe0a72722115603ac79c09
ShaunApps <shaun.apps@gmail.com>
2016-03-20 12:04:52 -0400

    configured mongoose and created first model

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+mongoose.connect('mongodb://localhost/news');
+
 var app = express();
 
 // view engine setup
