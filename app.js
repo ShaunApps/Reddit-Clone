21e2a663d2c80bccebc19bf20715295c0465d458
ShaunApps <shaun.apps@gmail.com>
2016-03-19 21:18:20 -0400

    binded , added ui-router

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 
 app.factory('posts', [function(){
@@ -13,13 +13,8 @@ app.controller('MainCtrl', [
   'posts',
   function($scope, posts){
     $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
+
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
