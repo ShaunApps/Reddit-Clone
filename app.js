e570b558cbc0614106c54fce70cabd1ba6a05ac7
ShaunApps <shaun.apps@gmail.com>
2016-03-19 20:07:27 -0400

    created a scope function that added an object into post array

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.controller('MainCtrl', [
       {title: 'post 2', upvotes: 2},
       {title: 'post 3', upvotes: 15},
       {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4},
+      {title: 'post 5', upvotes: 4}
     ];
+    $scope.addPost = function(){
+      $scope.posts.push({title: 'A new post!', upvotes: 0});
+    };
   }]);
