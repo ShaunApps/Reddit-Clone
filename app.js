af7384c31325cfb3ee8634cfce4f584809f5f833
ShaunApps <shaun.apps@gmail.com>
2016-03-20 12:07:57 -0400

    added second Comments model

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var mongoose = require('mongoose');
 require('./models/Posts');
+require('./models/Comments');
 mongoose.connect('mongodb://localhost/news');
 
 var app = express();
