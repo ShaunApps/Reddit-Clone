04f1f70c75dddef27bc00cda987c7ac4adae546d
ShaunApps <shaun.apps@gmail.com>
2016-03-21 15:43:24 -0400

    Create a route for preloading post objects in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
-
-
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
     if(err){ return next(err); }
@@ -30,3 +27,17 @@ router.post('/posts', function(req, res, next) {
     res.json(post);
   });
 });
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+module.exports = router;
