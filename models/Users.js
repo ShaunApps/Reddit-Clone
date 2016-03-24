45b9723bd0aa66c58413a013fb6a8f5b04d3cf05
ShaunApps <shaun.apps@gmail.com>
2016-03-24 15:47:14 -0400

    created a validPassword method in User model

--- models/Users.js
+++ models/Users.js
@@ -11,7 +11,11 @@ UserSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
 
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-
 };
 
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+}
 mongoose.model('User', UserSchema);
