f2e1066d43c20cec4ec52309bbf2e78041c21711
ShaunApps <shaun.apps@gmail.com>
2016-03-25 17:55:37 -0400

    added a login route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var passport = require('passport');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -99,6 +101,24 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
     if (err) { return next(err); }
 
     res.json(comment);
+  });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all the fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
   });
 });
 
