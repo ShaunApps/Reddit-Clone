2c0dcec10b4e6e9fc4d4d8cdf61a9b4989189be1
ShaunApps <shaun.apps@gmail.com>
2016-03-25 15:49:21 -0400

    added some notes on the User model

--- models/Users.js
+++ models/Users.js
@@ -8,12 +8,15 @@ var UserSchema = new mongoose.Schema({
   salt: String
 });
 
+// setPassword method that takes a ps and generates a salt and associated password hash
+// the pbkdf2Sync method takes 4 parameters: password, salt, iterations, and key length
 UserSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
 
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
+// this method takes the password and compares it to the hash stored, returns a boolean
 UserSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
