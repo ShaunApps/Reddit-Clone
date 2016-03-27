eb0cee028caeba9d63523d7f9fa9b70dc586e1f0
ShaunApps <shaun.apps@gmail.com>
2016-03-26 20:32:38 -0400

    added a controller for navbar

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -211,3 +211,12 @@ app.controller('MainCtrl', [
         });
       };
   }])
+
+  app.controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth){
+      $scope.isLoggedIn = auth.isLoggedIn;
+      $scope.currentUser = auth.currentUser;
+      $scope.logOut = auth.logOut;
+    }]);
